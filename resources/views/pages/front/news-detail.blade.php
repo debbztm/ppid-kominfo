448a6f99982a909353172d46620ac11334cdba74
Redha <tataredhaalfath13@gmail.com>
2023-12-10 11:26:32 +0700

    add share news to social

--- resources/views/pages/front/news-detail.blade.php
+++ resources/views/pages/front/news-detail.blade.php
@@ -17,6 +17,30 @@
                         <h6 class="ubuntu fz-13 gray-777 mt-20 mb-20">Dibuat oleh {{ $news->username }}</h6>
                         {!! $news->description !!}
                     </div>
+                    <div class="blog-single-share clearfix mt-50">
+                        <div class="pull-right text-center">
+                            <span class="martel text-bold fz-13 gray-777 text-uppercase">Share Informasi</span>
+                            <a href="https://www.facebook.com/sharer.php?u={{ Request::url() }}" target="_blank"
+                                rel="noreferrer" role="link">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                            <a href="https://twitter.com/share?url={{ Request::url() }}" target="_blank" rel="noreferrer"
+                                role="link">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            <a href="https://api.whatsapp.com/send?text={{ Request::url() }}" target="_blank"
+                                rel="noreferrer" role="link">
+                                <i class="fa fa-whatsapp"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url={{ Request::url() }}"
+                                target="_blank" rel="noreferrer" role="link">
+                                <i class="fa fa-linkedin va-2"></i>
+                            </a>
+                            <a href="mailto:?body={{ Request::url() }}" target="_blank" rel="noreferrer" role="link">
+                                <i class="fa fa-envelope"></i>
+                            </a>
+                        </div>
+                    </div>
                 @endif
             </div>
         </div>
